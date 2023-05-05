1b3d3919bd8b8da7e5e7883fa929034cbbb96410
OkkashaAlly <steamnature006@gmail.com>
2023-05-05 18:12:08 +0300

    APP: page.tsx - All asteroids collection page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,141 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+// COMPONENTS
+import { AsteroidCard, Loader } from "@/components/shared";
+import { H1, H2, H3 } from "@/components/typography";
+
+// ====================================
+// Home PAGE COMPONENTS ////////
+// ====================================
+export default function HomePage() {
+  const [asteroids, setAsteroids] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+  const [startDate, setStartDate] = useState("2015-09-07");
+  const [endDate, setEndDate] = useState("2015-09-08");
+
+  // CONSTANTS
+  const API_URL = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=DEMO_KEY`;
+
+  // fetching asteroids from
+  const fetchAsteroids = async () => {
+    // request options
+    const options = { method: "GET", headers: { accept: "application/json" } };
+    // error handling
+    const handleError = (err: any) => {
+      setError(err.message);
+      throw new Error(err);
+    };
+
+    try {
+      setLoading(true);
+      const res = await fetch(API_URL, options);
+      const data = await res.json();
+      console.log("Response: ", data);
+
+      if (data.error) handleError(data.error);
+
+      const asteroids = [
+        ...data.near_earth_objects[startDate],
+        ...data.near_earth_objects[endDate],
+      ];
+
+      setAsteroids(asteroids);
+      setLoading(false);
+    } catch (error: any) {
+      console.log("Error: ", error);
+      setLoading(false);
+    }
+  };
+
+  console.log("Asteroids: ", asteroids);
+
+  // fetch asteroids once (on page load)
+  useEffect(() => {
+    (async () => {
+      await fetchAsteroids();
+    })();
+  }, [startDate, endDate]);
+
+  // RETURN ///////////////////////////
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="p-6 pb-14 px-8 bg-zinc-900 min-h-screen">
+      <div className="max-w-screen-xl mx-auto">
+        {/*banner */}
+        <div className="">
+          <Banner />
+        </div>
+        {/*asteroids list */}
+        <div className="mt-8 px-1">
+          <div className="flex items-center justify-between flex-col md:flex-row">
+            <div className="flex items-baseline flex-col md:flex-row gap-2">
+              <H2 styles="text-white">Discover Latest asteroids Collection </H2>
+              <small className="text-gray-500 font-light">
+                (Please use the date range selectors to get collection of your
+                choice)
+              </small>
+            </div>
+            {/* selectors  */}
+            <div>
+              {/* date range selectors */}
+              <div className="flex items-center justify-center gap-2">
+                <div className="flex items-center justify-center gap-2">
+                  <H3 styles="text-white">From</H3>
+                  <input
+                    type="date"
+                    value={startDate}
+                    onChange={e => setStartDate(e.target.value)}
+                    className="bg-zinc-800 text-white rounded-lg p-2"
+                  />
+                </div>
+                <div className="flex items-center justify-center gap-2">
+                  <H3 styles="text-white">To</H3>
+                  <input
+                    type="date"
+                    value={endDate}
+                    onChange={e => setEndDate(e.target.value)}
+                    className="bg-zinc-800 text-white rounded-lg p-2"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mt-6">
+            {error ? (
+              <div className="flex justify-center">
+                <H3 styles="text-white">{error.message}</H3>
+              </div>
+            ) : loading ? (
+              <div className="flex justify-center">
+                <Loader />
+              </div>
+            ) : (
+              <div className="space-y-3">
+                {asteroids.map((asteroid: any) => (
+                  <AsteroidCard key={asteroid.id} info={asteroid} />
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+////////////////////////////////////////
+// EXTENDED COMPONENTS /////////////////
+const Banner = () => (
+  <div className="relative overflow-hidden flex items-center justify-center h-[200px] rounded-xl bg-gradient-to-r from-pink-500 to-purple-500">
+    <div className="absolute w-48 h-48 bg-white opacity-50 rounded-full -top-24 md:-top-9 -left-24 md:-left-12" />
+
+    <div className="flex flex-col items-center text-center justify-center space-y-3">
+      <H1 styles="text-white uppercase">asteroids</H1>
+      <H2 styles="text-white">The largest asteroids collection</H2>
+    </div>
+
+    <div className="absolute w-72 h-72 bg-white opacity-50 rounded-full -bottom-48 md:-bottom-24 -right-32 md:-right-12" />
+  </div>
+);
